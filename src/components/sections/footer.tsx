2975f23d93e2ef27710b11d6c46d2c0034dfe047
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-12-11 17:40:42 +0530

    remove: footer text fontweight animation

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -74,14 +74,18 @@ export default function Footer() {
       ref={footerRef}
       className="relative h-24 md:h-48 lg:h-56 xl:h-72 w-full overflow-hidden"
     >
-      <motion.h1
+      {/* <motion.h1
         ref={textRef}
         style={{ fontWeight }}
         transition={{ duration: 0.1 }}
         className="text-shadow-md font-sans dark:text-neutral-600 cursor-default select-none absolute bottom-0 left-0 right-0 text-6xl md:text-[140px] lg:text-[180px] xl:text-[210px] translate-y-[15%] tracking-tighter uppercase text-center mx-auto text-neutral-400"
       >
         dthstlkr
-      </motion.h1>
+      </motion.h1> */}
+
+      <h1 className="text-shadow-md font-sans font-black dark:text-neutral-600 cursor-default select-none absolute bottom-0 left-0 right-0 text-6xl md:text-[140px] lg:text-[180px] xl:text-[210px] translate-y-[15%] tracking-tighter uppercase text-center mx-auto text-neutral-400">
+        dthstlkr
+      </h1>
     </footer>
   );
 }
