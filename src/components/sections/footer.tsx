1811a08466cd48a7cd08f7e82a950fc2f3635e4f
0xdeathstalker <soumyamukherjee.work@gmail.com>
2026-03-21 10:30:20 +0530

    improve: footer background size, colours

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
         className="absolute top-0 bottom-0 left-1/2 -translate-x-1/2 z-0 w-full max-w-[650px] max-[690px]:w-[calc(100%-2rem)]"
         style={{
           background: "#fbfaf9",
-          backgroundImage: "radial-gradient(circle at 1px 1px, rgba(0, 0, 0, 0.35) 1px, transparent 0)",
-          backgroundSize: "18px 18px",
+          backgroundImage: "radial-gradient(circle at 1px 1px, rgba(89, 76, 91, 0.15) 1px, transparent 0)",
+          backgroundSize: "10px 10px",
         }}
       />
       <h1 className="font-pixel-square font-black dark:text-neutral-600 cursor-default select-none absolute inset-x-0 bottom-0 text-center uppercase tracking-tighter text-[oklch(72.8%_0.007_39.5)] text-shadow-md leading-[0.7] text-[clamp(4rem,18vw,7.5rem)]">
